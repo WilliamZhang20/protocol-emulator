8bdd0c1d86153499ca57769e741a7f164712482f
William Zhang <wzhang20@yahoo.com>
2026-09-15 16:35:10 -0400

    make test pass

--- src/project.v
+++ src/project.v
@@ -4,7 +4,7 @@
 
 `default_nettype none
 
-module tt_um_wzhang20_protocol_emulator (
+module tt_um_protocol_emulator (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
